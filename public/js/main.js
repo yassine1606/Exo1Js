acf0d08d57a294b3b43f5c64655e7da93078dee3
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-19 11:15:37 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+let maVariable;
+let maSecondeVariable;
+let maVariableInt;
+let ma2emeInt;
+
+let monTexte;
+let monTexte2;
+let varUne;
+let varDeux;
+
+
+maVariable = `J'aime les tartines et j'en mange`;
+maSecondeVariable =` 4 par jours`
+maVariableInt = 5;
+ma2emeInt = 35;
+monTexte = `Salut tout le monde`;
+console.log(monTexte);
+monTexte2 = `Aurevoir`
+
+monTexte = monTexte2;
+
+
+varUne=4;
+varDeux = `6`
+
+
+varUne>varDeux;
+varUne<varDeux;
+varUne==varDeux;
+varUne!=varDeux;
+varUne===varDeux;
+varUne!==varDeux;
+
+console.log(maVariable);
+
+console.log(maVariable+maSecondeVariable);
+
+console.log(maVariableInt+ma2emeInt);
+console.log(maVariable+maVariableInt);
+
+console.log(monTexte2);
+console.log(monTexte);
